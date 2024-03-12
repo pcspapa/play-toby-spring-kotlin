4b6ad9df81f06a8419afb5afed5ed20cf1535fcd
chanseok <pcspapa@gmail.com>
2024-03-12 17:15:08 +0900

    :tada: toby spring with kotlin

---
+++ src/main/kotlin/com/cspark/play/books/TobySpringKotlinApplication.kt
@@ -0,0 +1,11 @@
+package com.cspark.play.books
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TobySpringKotlinApplication
+
+fun main(args: Array<String>) {
+    runApplication<TobySpringKotlinApplication>(*args)
+}
---
+++ src/test/kotlin/com/cspark/play/books/TobySpringKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.cspark.play.books
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TobySpringKotlinApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
