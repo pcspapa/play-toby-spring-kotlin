5fcc29a60a7fa1d541858d9c47b569a5ab02bc2b
chanseok <pcspapa@gmail.com>
2024-03-12 17:25:33 +0900

    ✨ Create User class

---
+++ src/main/kotlin/com/cspark/play/books/user/User.kt
@@ -0,0 +1,17 @@
+package com.cspark.play.books.user
+
+class User(val id: String, val name: String, val password: String) {
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as User
+
+        return id == other.id
+    }
+
+    override fun hashCode(): Int {
+        return id.hashCode()
+    }
+}
---
+++ src/test/kotlin/com/cspark/play/books/user/UserTest.kt
@@ -0,0 +1,25 @@
+package com.cspark.play.books.user
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class UserTest {
+
+    @Test
+    fun createUser() {
+        val user = User("id", "name", "password")
+
+        assertThat(user).isNotNull()
+        assertThat(user.id).isEqualTo("id")
+        assertThat(user.name).isEqualTo("name")
+        assertThat(user.password).isEqualTo("password")
+    }
+
+    @Test
+    fun equalAndHash() {
+        val user = User("id", "name", "password")
+        val renameUser = User("id", "rename", "password")
+
+        assertThat(user).isEqualTo(renameUser)
+    }
+}
