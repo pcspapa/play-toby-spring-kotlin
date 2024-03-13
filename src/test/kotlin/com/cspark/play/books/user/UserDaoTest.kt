7bee6dd2f968d049066404665ed3f9e8bacbc626
chanseok <pcspapa@gmail.com>
2024-03-13 15:57:06 +0900

    ✅ Add count() in UserDaoTest

--- src/test/kotlin/com/cspark/play/books/user/UserDaoTest.kt
+++ src/test/kotlin/com/cspark/play/books/user/UserDaoTest.kt
@@ -19,19 +19,41 @@ class UserDaoTest {
         val user2 = User("bp", "Brad Pitt", "pw")
 
         dao.add(user1)
+        assertThat(dao.getCount()).isEqualTo(1)
 
         val findUser1: User = dao.get("mj")
-        assertThat(dao.getCount()).isEqualTo(1)
         assertThat(findUser1.id).isEqualTo("mj")
         assertThat(findUser1.name).isEqualTo("Mary Jane Watson")
         assertThat(findUser1.password).isEqualTo("pw")
 
         dao.add(user2)
+        assertThat(dao.getCount()).isEqualTo(2)
 
         val findUser2 = dao.get("bp")
-        assertThat(dao.getCount()).isEqualTo(2)
         assertThat(findUser2.id).isEqualTo("bp")
         assertThat(findUser2.name).isEqualTo("Brad Pitt")
         assertThat(findUser2.password).isEqualTo("pw")
+    }
+
+    @Test
+    fun count() {
+        val context: ApplicationContext = AnnotationConfigApplicationContext(DaoFactory::class.java)
+        val dao = context.getBean("userDao", UserDao::class.java)
+
+        dao.deleteAll()
+        assertThat(dao.getCount()).isEqualTo(0)
+
+        val user1 = User("mj", "Mary Jane Watson", "pw")
+        val user2 = User("bp", "Brad Pitt", "pw")
+        val user3 = User("papa", "Chanseok Park", "pw")
+
+        dao.add(user1)
+        assertThat(dao.getCount()).isEqualTo(1)
+
+        dao.add(user2)
+        assertThat(dao.getCount()).isEqualTo(2)
+
+        dao.add(user3)
+        assertThat(dao.getCount()).isEqualTo(3)
     }
 }
