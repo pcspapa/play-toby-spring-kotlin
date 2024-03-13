8324d8d5d189f3f393fc63630cd8d4cd31d5c22f
chanseok <pcspapa@gmail.com>
2024-03-13 14:03:02 +0900

    :white_check_mark: Create UserDaoTest

---
+++ src/test/kotlin/com/cspark/play/books/user/UserDaoTest.kt
@@ -0,0 +1,19 @@
+package com.cspark.play.books.user
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class UserDaoTest {
+
+    @Test
+    fun addAndGetNUser() { // Only test once
+        val dao = UserDao()
+
+        dao.add(User("mj", "Mary Jane Watson", "pw"))
+
+        val muserser: User = dao.get("mj")
+        assertThat(muserser.id).isEqualTo("mj")
+        assertThat(muserser.name).isEqualTo("Mary Jane Watson")
+        assertThat(muserser.password).isEqualTo("pw")
+    }
+}
